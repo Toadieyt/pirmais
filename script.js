d1245df18060f9c39d656896bbff9d5f134e96a9
Toadieyt <rudyblondyn6@gmail.com>
2023-01-01 21:23:10 +0100

    12323434

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ function showDiv(Div) {
     if(x.style.display=="none") {
         x.style.display = "flex";
         x.style.width = "100%";
-        x.style.gap = "20px";
+        x.style.gap = "30px";
         x.style.scrollSnapType = "x mandatory";
         x.style.overflowX = "auto";
+        x.style.marginTop = "80px";
     } else {
         x.style.display = "none";
     }
