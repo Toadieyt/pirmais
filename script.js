1df8526583494a80ff424efae34d1ea458a347f4
Toadieyt <rudyblondyn6@gmail.com>
2023-01-08 00:17:56 +0100

    123

--- script.js
+++ script.js
@@ -14,21 +14,3 @@ function showDiv(Div) {
         x.style.display = "none";
     }
 }
-
-function showDiv2(Div) {
-    let x = document.getElementById(Div);
-    if(x.style.display=="none") {
-        x.style.display = "flex";
-        x.style.justifyContent = "center";
-        x.style.width = "100%";
-        x.style.gap = "30px";
-        x.style.scrollSnapType = "x mandatory";
-        x.style.overflowX = "auto";
-        x.style.marginTop = "80px";
-        x.style.backgroundColor = "rgb(20, 20, 25)";
-        x.style.paddingTop = "40px";
-        x.style.paddingBottom = "40px";
-    } else {
-        x.style.display = "none";
-    }
-}
